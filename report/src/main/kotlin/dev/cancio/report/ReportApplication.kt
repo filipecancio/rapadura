c8d5eae88b7f28c2d732b18a3b8e7273ad36df07
cancio <filipe.cancio@outlook.com>
2021-11-30 01:48:57 -0300

    modulo de relatorio

---
+++ report/src/main/kotlin/dev/cancio/report/ReportApplication.kt
@@ -0,0 +1,11 @@
+package dev.cancio.report
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReportApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReportApplication>(*args)
+}
